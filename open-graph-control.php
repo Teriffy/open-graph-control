242e5baedcf33c0239b26eaf1ad6a4a62b728b06
Evžen Leonenko <teriffy@gmail.com>
2026-04-19 04:15:27 +0200

    feat: plugin bootstrap header

---
+++ open-graph-control.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:       Open Graph Control
+ * Plugin URI:        https://wordpress.org/plugins/open-graph-control/
+ * Description:       Full control over Open Graph and social meta tags across 12 platforms.
+ * Version:           0.0.1
+ * Requires at least: 6.2
+ * Requires PHP:      8.0
+ * Author:            Evžen Leonenko
+ * Author URI:        https://leonenko.cz
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * Text Domain:       open-graph-control
+ * Domain Path:       /languages
+ *
+ * @package EvzenLeonenko\OpenGraphControl
+ */
+
+declare(strict_types=1);
+
+defined( 'ABSPATH' ) || exit;
+
+// PSR-4 autoload.
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Constants.
+define( 'OGC_VERSION', '0.0.1' );
+define( 'OGC_FILE', __FILE__ );
+define( 'OGC_DIR', plugin_dir_path( __FILE__ ) );
+define( 'OGC_URL', plugin_dir_url( __FILE__ ) );
